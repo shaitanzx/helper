827892b62f468b88670293d5e6a25d8f0df5625e
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-09-15 18:04:22 +0400

    Update __init__.py

--- onebuttonprompt/__init__.py
+++ onebuttonprompt/__init__.py
@@ -6,8 +6,6 @@ import sys
 custom_nodes_path = os.path.join(os.path.abspath(__file__), "custom_nodes")
 ##onebuttonprompt_path = os.path.join(custom_nodes_path, "OneButtonPrompt")
 onebuttonprompt_path = os.path.join(os.path.abspath(__file__), "onebuttonprompt")
-print('custom_nodes_path=',custom_nodes_path)
-print('onebuttonprompt_path=',onebuttonprompt_path)
 sys.path.append(onebuttonprompt_path)
 
 from .OneButtonPromptNodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
