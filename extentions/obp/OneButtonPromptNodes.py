c693609a6a5cef8c1db3d05f0e6bbc2aa9044d8d
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-09-19 01:25:47 +0400

    2

--- extentions/obp/OneButtonPromptNodes.py
+++ extentions/obp/OneButtonPromptNodes.py
@@ -9,8 +9,7 @@ import platform
 custom_nodes_path = os.path.join(os.path.abspath(__file__), "custom_nodes")
 ##onebuttonprompt_path = os.path.join(custom_nodes_path, "OneButtonPrompt")
 onebuttonprompt_path = os.path.join(os.path.abspath(__file__), "onebuttonprompt")
-print('custom_nodes_path=',custom_nodes_path)
-print('onebuttonprompt_path=',onebuttonprompt_path)
+
 
 sys.path.append(onebuttonprompt_path)
 
