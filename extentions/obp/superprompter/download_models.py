505cae2b9dfc0e94da28f712b2be35766d2e5f27
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-09-27 17:12:11 +0400

    Update download_models.py

--- extentions/obp/superprompter/download_models.py
+++ extentions/obp/superprompter/download_models.py
@@ -1,13 +1,14 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 import os
+import sentencepiece
 
 def download_models():
     model_name = "roborovski/superprompt-v1"
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.float16)
     script_dir = os.path.dirname(os.path.abspath(__file__))  # Script directory
-    modelDir = os.path.join(script_dir, "./model_files/" )
+    modelDir = os.path.join(script_dir, "model_files" )
     os.makedirs(modelDir, exist_ok=True)
     tokenizer.save_pretrained(modelDir)
     model.save_pretrained(modelDir)
