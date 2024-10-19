ad988a836c7801ab8388f2e4b84549dc8d773d1d
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-10-19 15:01:15 +0400

    0

--- webui.py
+++ webui.py
@@ -929,9 +929,9 @@ with shared.gradio_root:
 			
             with gr.Row(elem_classes='extend_row'):
                 with gr.Accordion('Extention', open=False):
-                  from extentions.abg import app
-                  abg_ext=abg()
-                  with gr.TabItem(label=abg_ext.title()):
+                  from extentions.adetailer import adetailer
+                  adetailer=AfterDetailerScript()
+                  with gr.TabItem(label=adetailer.title()):
                     body_estimation = None
 
                   with gr.TabItem(label='OpenPoseEditor', elem_id='op_edit_tab') as op_edit_tab:
