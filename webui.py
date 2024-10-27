248793cdd3d2f064af36d1af4d6a9cc001f8e86f
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-10-27 11:11:39 +0400

    1

--- webui.py
+++ webui.py
@@ -955,112 +955,6 @@ with shared.gradio_root:
 			
             with gr.Row(elem_classes='extend_row'):
                 with gr.Accordion('Extention', open=False):
-                  with gr.TabItem(label='OpenPoseEditor', elem_id='op_edit_tab') as op_edit_tab:
-                    body_estimation = None
-                    presets_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "extentions", "op_edit","presets.json")
-                    presets = {}
-
-                    try: 
-                      with open(presets_file) as file:
-                        presets = json.load(file)
-                    except FileNotFoundError:
-                      pass
-
-                    def pil2cv(in_image):
-                      out_image = np.array(in_image, dtype=np.uint8)
-
-                      if out_image.shape[2] == 3:
-                          out_image = cv2.cvtColor(out_image, cv2.COLOR_RGB2BGR)
-                      return out_image
-
-                    def candidate2li(li):
-                      res = []
-                      for x, y, *_ in li:
-                        res.append([x, y])
-                      return res
-
-                    def subset2li(li):
-                      res = []
-                      for r in li:
-                        for c in r:
-                          res.append(c)
-                      return res                    
-                    with gr.Row():
-                      with gr.Column():
-                        width_ope = gr.Slider(label="width", minimum=64, maximum=2048, value=512, step=64, interactive=True)
-                        height_ope = gr.Slider(label="height", minimum=64, maximum=2048, value=512, step=64, interactive=True)
-                        with gr.Row():
-                          add_ope = gr.Button(value="Add", variant="primary")
-                          # delete = gr.Button(value="Delete")
-                        with gr.Row():
-                          reset_btn_ope = gr.Button(value="Reset")
-                          json_input_ope = gr.UploadButton(label="Load from JSON", file_types=[".json"], elem_id="openpose_json_button")
-                          png_input_ope = gr.UploadButton(label="Detect from Image", file_types=["image"], type="bytes", elem_id="openpose_detect_button")
-                          bg_input_ope = gr.UploadButton(label="Add Background Image", file_types=["image"], elem_id="openpose_bg_button")
-                        with gr.Row():
-                          preset_list = gr.Dropdown(label="Presets", choices=sorted(presets.keys()), interactive=True)
-                          preset_load = gr.Button(value="Load Preset")
-                          preset_save = gr.Button(value="Save Preset")
-                        with gr.Row():
-                          json_output_ope = gr.Button(value="Save JSON")
-                          png_output_ope = gr.Button(value="Save PNG")
-                      with gr.Column():
-        # gradioooooo...
-                        canvas_ope = gr.HTML('<canvas id="openpose_editor_canvas" width="512" height="512" style="margin: 0.25rem; border-radius: 0.25rem; border: 0.5px solid"></canvas>')
-                        jsonbox_ope = gr.Text(label="json", elem_id="jsonbox", visible=False)
-                    def estimate(file):
-                      global body_estimation
-
-                      if body_estimation is None:
-                        model_path_ope = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", "controlnet")
-                        model_file_ope = os.path.join(model_path_ope, "body_pose_model.pth")
-                        if not os.path.isfile(model_path_ope):
-
-                          load_file_from_url(
-                            url="https://huggingface.co/shaitanzx/FooocusExtend/resolve/main/body_pose_model.pth",
-                            model_dir=model_path_ope,
-                            file_name='body_pose_model.pth')
-
-
-                        body_estimation = Body(model_file_ope)
-        
-                      stream = io.BytesIO(file)
-                      img = Image.open(stream)
-                      candidate, subset = body_estimation(pil2cv(img))
-
-                      result = {
-                        "candidate": candidate2li(candidate),
-                        "subset": subset2li(subset),
-                      }
-      
-                      return str(result).replace("'", '"')
-
-                    def savePreset(name, data):
-                      if name:
-                        presets[name] = json.loads(data)
-                        with open(presets_file, "w") as file:
-                          json.dump(presets, file)
-                        return gr.update(choices=sorted(presets.keys()), value=name), json.dumps(data)
-                      return gr.update(), gr.update()
-
-                    dummy_component = gr.Label(visible=False)
-
-                    preset_ope = gr.Text(visible=False)
-                    width_ope.change(None, [width_ope, height_ope], None, _js="(w, h) => {resizeCanvas(w, h)}")
-                    height_ope.change(None, [width_ope, height_ope], None, _js="(w, h) => {resizeCanvas(w, h)}")
-                    png_output_ope.click(None, [], None, _js="savePNG")
-                    bg_input_ope.upload(None, [], [width_ope, height_ope], _js="() => {addBackground('openpose_bg_button')}")
-                    png_input_ope.upload(estimate, png_input_ope, jsonbox_ope)
-                    png_input_ope.upload(None, [], [width_ope, height_ope], _js="() => {addBackground('openpose_detect_button')}")
-                    add_ope.click(None, [], None, _js="addPose")
-                    reset_btn_ope.click(None, [], None, _js="resetCanvas")
-                    json_input_ope.upload(None, json_input_ope, [width_ope, height_ope], _js="() => {loadJSON('openpose_json_button')}")
-                    json_output_ope.click(None, None, None, _js="saveJSON")
-                    preset_save.click(savePreset, [dummy_component, dummy_component], [preset_list, preset_ope], _js="savePreset")
-                    preset_load.click(None, preset_ope, [width_ope, height_ope], _js="loadPreset")
-                    preset_list.change(lambda selected: json.dumps(presets[selected]), preset_list, preset_ope)
-
-
                   with gr.TabItem(label='OBP') as obp_tab:
                         with gr.Tab("Main"):
                             with gr.Row(variant="compact"):
@@ -1299,6 +1193,110 @@ with shared.gradio_root:
                             with gr.Row():            
                                 p_n_tr = gr.Textbox(label='Negative Translate', show_label=False, value='', lines=2, placeholder='Translated negative text prompt')             
                     gr.HTML('* \"Prompt Translate\" is powered by AlekPet. <a href="https://github.com/AlekPet/Fooocus_Extensions_AlekPet" target="_blank">\U0001F4D4 Document</a>')
+                  with gr.TabItem(label='OpenPoseEditor', elem_id='op_edit_tab') as op_edit_tab:
+                    body_estimation = None
+                    presets_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "extentions", "op_edit","presets.json")
+                    presets = {}
+
+                    try: 
+                      with open(presets_file) as file:
+                        presets = json.load(file)
+                    except FileNotFoundError:
+                      pass
+
+                    def pil2cv(in_image):
+                      out_image = np.array(in_image, dtype=np.uint8)
+
+                      if out_image.shape[2] == 3:
+                          out_image = cv2.cvtColor(out_image, cv2.COLOR_RGB2BGR)
+                      return out_image
+
+                    def candidate2li(li):
+                      res = []
+                      for x, y, *_ in li:
+                        res.append([x, y])
+                      return res
+
+                    def subset2li(li):
+                      res = []
+                      for r in li:
+                        for c in r:
+                          res.append(c)
+                      return res                    
+                    with gr.Row():
+                      with gr.Column():
+                        width_ope = gr.Slider(label="width", minimum=64, maximum=2048, value=512, step=64, interactive=True)
+                        height_ope = gr.Slider(label="height", minimum=64, maximum=2048, value=512, step=64, interactive=True)
+                        with gr.Row():
+                          add_ope = gr.Button(value="Add", variant="primary")
+                          # delete = gr.Button(value="Delete")
+                        with gr.Row():
+                          reset_btn_ope = gr.Button(value="Reset")
+                          json_input_ope = gr.UploadButton(label="Load from JSON", file_types=[".json"], elem_id="openpose_json_button")
+                          png_input_ope = gr.UploadButton(label="Detect from Image", file_types=["image"], type="bytes", elem_id="openpose_detect_button")
+                          bg_input_ope = gr.UploadButton(label="Add Background Image", file_types=["image"], elem_id="openpose_bg_button")
+                        with gr.Row():
+                          preset_list = gr.Dropdown(label="Presets", choices=sorted(presets.keys()), interactive=True)
+                          preset_load = gr.Button(value="Load Preset")
+                          preset_save = gr.Button(value="Save Preset")
+                        with gr.Row():
+                          json_output_ope = gr.Button(value="Save JSON")
+                          png_output_ope = gr.Button(value="Save PNG")
+                      with gr.Column():
+        # gradioooooo...
+                        canvas_ope = gr.HTML('<canvas id="openpose_editor_canvas" width="512" height="512" style="margin: 0.25rem; border-radius: 0.25rem; border: 0.5px solid"></canvas>')
+                        jsonbox_ope = gr.Text(label="json", elem_id="jsonbox", visible=False)
+                    def estimate(file):
+                      global body_estimation
+
+                      if body_estimation is None:
+                        model_path_ope = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", "controlnet")
+                        model_file_ope = os.path.join(model_path_ope, "body_pose_model.pth")
+                        if not os.path.isfile(model_path_ope):
+
+                          load_file_from_url(
+                            url="https://huggingface.co/shaitanzx/FooocusExtend/resolve/main/body_pose_model.pth",
+                            model_dir=model_path_ope,
+                            file_name='body_pose_model.pth')
+
+
+                        body_estimation = Body(model_file_ope)
+        
+                      stream = io.BytesIO(file)
+                      img = Image.open(stream)
+                      candidate, subset = body_estimation(pil2cv(img))
+
+                      result = {
+                        "candidate": candidate2li(candidate),
+                        "subset": subset2li(subset),
+                      }
+      
+                      return str(result).replace("'", '"')
+
+                    def savePreset(name, data):
+                      if name:
+                        presets[name] = json.loads(data)
+                        with open(presets_file, "w") as file:
+                          json.dump(presets, file)
+                        return gr.update(choices=sorted(presets.keys()), value=name), json.dumps(data)
+                      return gr.update(), gr.update()
+
+                    dummy_component = gr.Label(visible=False)
+
+                    preset_ope = gr.Text(visible=False)
+                    width_ope.change(None, [width_ope, height_ope], None, _js="(w, h) => {resizeCanvas(w, h)}")
+                    height_ope.change(None, [width_ope, height_ope], None, _js="(w, h) => {resizeCanvas(w, h)}")
+                    png_output_ope.click(None, [], None, _js="savePNG")
+                    bg_input_ope.upload(None, [], [width_ope, height_ope], _js="() => {addBackground('openpose_bg_button')}")
+                    png_input_ope.upload(estimate, png_input_ope, jsonbox_ope)
+                    png_input_ope.upload(None, [], [width_ope, height_ope], _js="() => {addBackground('openpose_detect_button')}")
+                    add_ope.click(None, [], None, _js="addPose")
+                    reset_btn_ope.click(None, [], None, _js="resetCanvas")
+                    json_input_ope.upload(None, json_input_ope, [width_ope, height_ope], _js="() => {loadJSON('openpose_json_button')}")
+                    json_output_ope.click(None, None, None, _js="saveJSON")
+                    preset_save.click(savePreset, [dummy_component, dummy_component], [preset_list, preset_ope], _js="savePreset")
+                    preset_load.click(None, preset_ope, [width_ope, height_ope], _js="loadPreset")
+                    preset_list.change(lambda selected: json.dumps(presets[selected]), preset_list, preset_ope)
 
                   with gr.TabItem(label='Photopea') as photopea_tab:
                     PHOTOPEA_MAIN_URL = 'https://www.photopea.com/'
