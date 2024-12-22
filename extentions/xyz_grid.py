70641e454772b140e596d3eb9d1d87ad98fb1d6a
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-12-22 19:15:12 +0400

    2

--- extentions/xyz_grid.py
+++ extentions/xyz_grid.py
@@ -368,7 +368,15 @@ def draw_grid(x_labels,y_labels,z_labels,list_size,ix,iy,iz,draw_legend,xs,ys,zs
                   (title_text_width, title_text_height), _ = cv2.getTextSize(title_text[z], font, font_scale, thickness)
                   cv2.putText(image_extended, title_text[z], (int((image_extended.shape[1]-title_text_width)/2),20+title_text_height), font, font_scale, text_color, thickness)
                   wall=image_extended
-          
+        meta_xyz=[('Prompt','prompt',currentTask.args[1])]
+        if hor_text[0]:
+            for i in range(len(hor_text)):
+              meta_xyz.append((f'X axis {i+1}:', 'X axis', hor_text[i]))
+        if vert_text[0]:
+            for i in range(len(vert_text)):
+              meta_xyz.append((f'Y axis {i+1}:', 'Y axis', vert_text[i]))
+        if title_text[z]:
+            meta_xyz.append((f'Z axis:', 'Z axis', title_text[z]))
 
         log(wall, metadata=[('Grid', 'Grid', 'Grid')], metadata_parser=None, output_format=None, task=None, persist_image=True)
 
